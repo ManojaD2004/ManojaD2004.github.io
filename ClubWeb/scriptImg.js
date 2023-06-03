81f96bc51f5b4079e8ffbb1436d2d7b63cc6e32d
ManojaD2004 <72197799+ManojaD2004@users.noreply.github.com>
2023-06-03 15:55:24 +0530

    Add files via upload

---
+++ ClubWeb/scriptImg.js
@@ -0,0 +1,8 @@
+let imgSCase = document.getElementById("imgSCase");
+let imgSCaseL = document.getElementById("imgSCaseL");
+let imgSCaseM = document.getElementById("imgSCaseM");
+let imgSCaseR = document.getElementById("imgSCaseR");
+
+function imgSCaseLFun() {
+    imgSCaseR.style.backgroundColor = "red";
+}
