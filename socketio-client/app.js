b67cb9e02276ac7da10daa93da5068e832a7eca3
Manoja D <manojadkc2004@gmail.com>
2024-01-15 14:21:48 +0530

    Add files via upload

--- socketio-client/app.js
+++ socketio-client/app.js
@@ -1,14 +1,20 @@
 const socket = io("https://socket-io-learn-production.up.railway.app", {
   withCredentials: true,
 });
+let time, newTime;
 
 socket.on("message", (text) => {
+  if (time !== null && time !== undefined) {
+    newTime = Date.now();
+    console.log(`It Took ${(newTime - time) / 1000}`);
+  }
   const el = document.createElement("li");
   el.innerHTML = text;
   document.querySelector("ul").appendChild(el);
 });
 
 document.querySelector("button").onclick = () => {
+  time = Date.now();
   const text = document.querySelector("input").value;
   socket.emit("message", text);
 };
